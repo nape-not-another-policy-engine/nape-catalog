f63e18a656523001dd86ca47ebb381b21daf8029
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2024-10-02 14:33:49 +0200

    iupdated procedure

--- nape-tutorials/tutorial-1/simple-sdlc-process/continuous-integration/activity/app-testing/unit_test_gt_80.py
+++ nape-tutorials/tutorial-1/simple-sdlc-process/continuous-integration/activity/app-testing/unit_test_gt_80.py
@@ -1,6 +1,6 @@
 def evaluate(evidence):
     try:
-        return "fail", r"CVE NO HIGH Needs Implementation"
+        return "fail", r"Unit Test Greater than 80% Needs Implementation"
     except Exception as e:
         # Return an error outcome with the error message
         return "error", f"An error occurred during evaluation: {str(e)}"
