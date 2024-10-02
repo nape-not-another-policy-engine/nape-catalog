0716c9d15fc32e322719310d23ab831069a4e179
Bill Bensing <10779425+BillBensing@users.noreply.github.com>
2024-10-02 14:32:06 +0200

    saved chagnes

--- nape-tutorials/tutorial-1/simple-sdlc-process/continuous-integration/activity/peer-review/at_least_two_reviewers.py
+++ nape-tutorials/tutorial-1/simple-sdlc-process/continuous-integration/activity/peer-review/at_least_two_reviewers.py
@@ -1,11 +1,16 @@
 import json
 
-def evaluate(lines):
+def evaluate(evidence):
     min_pr_reviewers = 2
-    pull_request = json.loads(''.join(lines))
-    reviewers = pull_request['requested_reviewers']
-    if len(reviewers) >= min_pr_reviewers:
-        return 'pass', f'There are {len(reviewers)} PR Reviewers.'
-    else:
-        return 'fail', (f'There are only {len(reviewers)} PR Reviewers, when there needs to '
-                          f'be a minimum of {min_pr_reviewers} PR Reviewers.')
+    try:
+        pull_request = json.loads(''.join(evidence))
+        reviewers = pull_request['requested_reviewers']
+        if len(reviewers) >= min_pr_reviewers:
+            return 'pass', f'There are {len(reviewers)} PR Reviewers.'
+        else:
+            return 'fail', (f'There are only {len(reviewers)} PR Reviewers, when there needs to '
+                            f'be a minimum of {min_pr_reviewers} PR Reviewers.')
+    
+    except Exception as e:
+        # Return an error outcome with the error message
+        return "error", f"An error occurred during evaluation: {str(e)}"
